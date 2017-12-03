ffb1d2cab264477c9e49783c7988541b2bb3bdda
Bruno Annunciato <brunobannunciato@gmail.com>
2017-12-03 01:17:55 -0200

    [ADD] PWA

---
+++ src/sw.js
@@ -0,0 +1,46 @@
+var CACHE_NAME = 'static-v1';
+
+self.addEventListener('install', function (event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function (cache) {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        'css/styles.css',
+        'pages/alude.html',
+        'pages/bullet-bane.html',
+        'pages/buscar.html',
+        'pages/dead-fish.html',
+        'pages/galeria.html',
+        'pages/garage-fuzz.html',
+        'pages/pense.html',
+        'pages/programas.html',
+        'scripts/app.min.js',
+        '/manifest.json',
+      ]);
+    })
+  )
+});
+
+self.addEventListener('activate', function activator(event) {
+  event.waitUntil(
+    caches.keys().then(function (keys) {
+      return Promise.all(keys
+        .filter(function (key) {
+          return key.indexOf(CACHE_NAME) !== 0;
+        })
+        .map(function (key) {
+          return caches.delete(key);
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', function (event) {
+  event.respondWith(
+    caches.match(event.request).then(function (cachedResponse) {
+      return cachedResponse || fetch(event.request);
+    })
+  );
+});
